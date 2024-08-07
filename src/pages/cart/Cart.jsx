16f7a67067e0e11ea6dc799cea4936ca30ec4e9a
Haley Massa <haleyemassa@gmail.com>
2024-08-07 12:12:32 -0500

    small UI changes

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -2,9 +2,14 @@ import { useSelector } from "react-redux";
 import { selectCartContents } from "../../redux/cartSlice";
 import { CartItem } from "../../components/cartItem/CartItem";
 import "./Cart.css";
+import { useMemo } from "react";
 
 export const Cart = () => {
     const cartContents = useSelector(selectCartContents);
+    const totalCost = useMemo(() => {
+        return cartContents.reduce((acc, item) => acc + item.price, 0)
+    }, [cartContents]);
+    
     return (
         <div className="cart-container">
             <h2>{"My Cart"}</h2>
@@ -15,8 +20,9 @@ export const Cart = () => {
                             return <CartItem key={i} {...item} />;
                         })}
                     </div>
-                    <div className="checkout-button">
-                        <button type="button">{"Checkout"}</button>
+                    <div className="checkout-section">
+                        <p>Total: ${totalCost}</p>
+                        <button type="button">{"Checkout with Stripe"}</button>
                     </div>
                 </>
             ) : (
